8a082dbbcfddba8b0d8405606266df39f1410750
Ekaterina Baliasnikova <Ekaterina.Baliasnikova@ecentria.com>
2022-09-29 09:40:28 +0300

    fix broken initial categories

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,48 +1,56 @@
 import * as React from 'react'
 import Question from '../../features/Question/Question'
 import Joke from '../../components/Joke/Joke'
+import Error from '../../components/Error/Error'
 import ShowMoreButton from '../../features/ShowMoreButton/ShowMoreButton'
 import Box from '@mui/material/Box'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../app/store'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { fetchCategories } from '../../app/appSlice'
+import { unwrapResult } from '@reduxjs/toolkit'
 import { useAppDispatch } from '../../app/hooks'
 import Chuck from '../../components/Chuck/Chuck'
-import Error from '../../components/Error/Error'
 
 const Main: React.FC = () => {
   const dispatch = useAppDispatch()
-  const { previousCategory, joke, error, categories } = useSelector(
+  const { previousCategory, joke, error } = useSelector(
     (state: RootState) => state.data
   )
+  const [categories, setCategories] = useState([])
 
   useEffect(() => {
     void dispatch(fetchCategories())
+      .then(unwrapResult)
+      .then((originalPromiseResult) => {
+        setCategories(originalPromiseResult)
+      })
   }, [])
 
   return (
     <Chuck>
-        {error
-          ? <Error errorText={error}/>
-          : (<>
-            <Question
-              previousCategory={previousCategory}
-              categories={categories}
-            />
-            {joke.value && (
-              <Box sx={{ width: { md: '50%', xs: '100%' }, mt: 3, mb: { xs: 2, md: 9 } }}>
-                <Joke value={joke.value} />
-              </Box>
-            )}
-            <Box mt={3} mb={{ xs: 2, md: 7 }} sx={{ width: { xs: '100%', md: '50%' } }}>
-              <ShowMoreButton
+        {/* @TODO: think about conditions for error, it should replace button and title */}
+        {error && <Error errorText={error} />}
+        {categories.length > 0 && (
+            <>
+              <Question
                 previousCategory={previousCategory}
-                initialCategory={categories[0]}
+                categories={categories}
               />
-            </Box>
-          </>)
-        }
+              {error !== null && 'Sorry, no jokes. Try to reload page'}
+              {joke.value && (
+                <Box sx={{ width: { md: '50%', xs: '100%' }, mt: 3, mb: { xs: 2, md: 9 } }}>
+                  <Joke value={joke.value} />
+                </Box>
+              )}
+              <Box mt={3} mb={{ xs: 2, md: 7 }} sx={{ width: { xs: '100%', md: '50%' } }}>
+                <ShowMoreButton
+                  previousCategory={previousCategory}
+                  initialCategory={categories[0]}
+                />
+              </Box>
+          </>
+        )}
     </Chuck>
   )
 }
